835e01ce33460d6b6df447f1cd2ee5b5c0726c69
Debora Kwon <95396914+Debora-k@users.noreply.github.com>
2022-10-03 19:03:45 -0600

    Updated documentation and types for InsertionSort

--- A1Group5/src/algorithms/InsertionSort.java
+++ A1Group5/src/algorithms/InsertionSort.java
@@ -1,20 +1,34 @@
 package algorithms;
 
+/**
+ * A util class for sorting a Comparable array using insertion sort algorithm.
+ * @author Debora Kwon
+ *
+ */
 public class InsertionSort {
-	public Comparable[] insertionSort (Comparable[] arr) {
+	
+	/**
+	 * Runs insertion sort algorithm in-place on an array
+	 * @param arr A collection of Comparables
+	 * @return The same array that was sorted
+	 */
+	public <T extends Comparable<? super T>> T[] insertionSort (T[] arr) {
 		
-		for(int i= 0; i < arr.length - 1; i++) {
+		// go through each element except the last element
+		for(int i = 0; i < arr.length - 1; i++) {
+			// set up another loop to track moving the compared element to the left
 			for(int j = i; j >= 0; j--) {
-			// sorting greatest to least
-			// when arr[i] is less than arr[i+1] -> negative number
+				// sorting greatest to least
+				// when arr[j] is less than arr[j + 1] -> negative number
 				if(arr[j].compareTo(arr[j + 1]) < 0) {
-					// if i+1 is greater than i, then swap the elements
-					Comparable tempValue;
+					// if arr[j + 1] is greater than arr[j], then swap the elements
+					T tempValue;
 					tempValue = arr[j + 1];
 					arr[j + 1] = arr[j];
 					arr[j] = tempValue;
 				
 				} else {
+					// otherwise, don't need to keep moving left
 					break;
 				}
 			}
